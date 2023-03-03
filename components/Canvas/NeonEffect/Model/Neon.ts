7784758bdb6e6b2d3c692cb1ac35be14d410df34
kunal1400 <kunal.malviya@team.telstra.com>
2023-03-03 11:24:47 +0530

    Fixed the ts issue

--- components/Canvas/NeonEffect/Model/Neon.ts
+++ components/Canvas/NeonEffect/Model/Neon.ts
@@ -9,27 +9,26 @@ export interface ICanvasTextGraphics {
 export class Neon {
     sketch: p5Types;
     inputFontSize: number = 30;
+    textGraphics: p5Types.Graphics;
 
     constructor(sketch: p5Types) {
         this.sketch = sketch;
+        this.textGraphics = this.sketch.createGraphics(sketch.width, sketch.height);
     }
 
     getTextGraphic = ({w, h, t}: ICanvasTextGraphics) => {
-        let textGraphics: p5Types.Graphics;
-        textGraphics = this.sketch.createGraphics(w, h);
-      
         //first push
-        textGraphics.push();
+        this.textGraphics.push();
         // It is necessary to set textSize & textLeading equal for proper calculations.
-        textGraphics.textSize(this.inputFontSize);
-        textGraphics.textLeading(this.inputFontSize);
+        this.textGraphics.textSize(this.inputFontSize);
+        this.textGraphics.textLeading(this.inputFontSize);
         
-        let textGraphicsWidth = textGraphics.width;
-        let textGraphicsHeight = textGraphics.height;
+        let textGraphicsWidth = this.textGraphics.width;
+        let textGraphicsHeight = this.textGraphics.height;
         
-        let tw = this.sketch.round(textGraphics.textWidth(t));
+        let tw = this.sketch.round(this.textGraphics.textWidth(t));
         // let tw = textToDisplay.length;  
-        let ts = textGraphics.textSize();
+        let ts = this.textGraphics.textSize();
         let numberOfLines = t.split(/\r\n|\r|\n/).length;  
         let currentRowInCanvas = this.sketch.round(tw/textGraphicsWidth) + numberOfLines;
         let maximumRowInCanvas = this.sketch.round(textGraphicsHeight/ts);
@@ -39,25 +38,26 @@ export class Neon {
           this.getTextGraphic({w, h, t});
         }
         
-        textGraphics.noFill();
-        textGraphics.stroke(0);
-        textGraphics.strokeWeight(2);
+        this.textGraphics.noFill();
+        this.textGraphics.stroke(0);
+        this.textGraphics.strokeWeight(2);
         
-        const tdc = textGraphics.drawingContext;
-        textGraphics.drawingContext.shadowBlur = 10;
-        textGraphics.drawingContext.shadowColor = 'black';
+        // Get the drawing context of the buffer
+        const context = this.textGraphics.drawingContext as CanvasRenderingContext2D;
+        context.shadowBlur = 10;
+        context.shadowColor = 'black';
     
-        textGraphics.textSize(this.inputFontSize);
-        textGraphics.textLeading(this.inputFontSize);
-        textGraphics.textAlign("center");
-        textGraphics.text(t, 0, 0, textGraphicsWidth, textGraphicsHeight);
+        this.textGraphics.textSize(this.inputFontSize);
+        this.textGraphics.textLeading(this.inputFontSize);
+        this.textGraphics.textAlign("center");
+        this.textGraphics.text(t, 0, 0, textGraphicsWidth, textGraphicsHeight);
         
-        textGraphics.remove();
-        textGraphics.pop();
+        this.textGraphics.remove();
+        this.textGraphics.pop();
     
         this.sketch.imageMode("center");
-        this.sketch.image(textGraphics, this.sketch.width/2, this.sketch.height/2);
+        this.sketch.image(this.textGraphics, this.sketch.width/2, this.sketch.height/2);
     
-        return textGraphics;
+        return this.textGraphics;
     }
 }

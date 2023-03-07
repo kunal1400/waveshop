ca873f83cce97f5a58a145fc0fd31cd2c4050c6f
kunal1400 <kunal.malviya@team.telstra.com>
2023-03-07 17:56:34 +0530

    Render canvas

---
+++ components/Canvas/index.tsx
@@ -0,0 +1,2 @@
+export {default as BorderShadow} from "./BorderShadow/Component";
+export {default as NeonEffect} from "./NeonEffect/Component";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
+import dynamic from "next/dynamic";
 import {DesignNavigation, DesignSidebar} from "../components/Layout";
+
+// Dynamically loading canvas because its api depend on window object
+const NeonEffect = dynamic(() => import("../components/Canvas/NeonEffect/Component"), {
+    ssr: false
+})
 
 export default function Index() {
     return <>
         <DesignNavigation/>
         <div className="flex">
             <DesignSidebar/>
-            <div>Canvas</div>
+            <NeonEffect/>
         </div>
     </>
 }

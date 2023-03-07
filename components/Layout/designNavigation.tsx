cb6a358ae5eb3fe08d6eb34196c6d0a325e8e112
kunal1400 <kunal.malviya@team.telstra.com>
2023-03-07 11:50:20 +0530

    Design page UI

--- components/Layout/designNavigation.tsx
+++ components/Layout/designNavigation.tsx
@@ -1,12 +1,18 @@
 import { LayoutPropsType } from "../../interfaces";
 import Link from "next/link";
 
+/**
+ * Button component for Design Navigation
+ */
 export function NavButton({children}: LayoutPropsType) {
     return <button className="p-2 text-slate-50 hover:text-slate-300">
         {children}
     </button>
 }
 
+/**
+ * Design Navigation UI
+ */
 export default function DesignNavigation() {
     return <div className="w-full p-2 bg-gradient-to-r from-purple-500 via-blue-500 to-green-600">
         <div className="inline-flex justify-start w-full sm:w-1/2">
